54791d916df79bb14561505d68b32c34830c813e
hb1007 <himanshu.bisht430@gmail.com>
2022-06-16 16:24:49 +0530

    first commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Skate from './Skate.gif'
+
+const Spinner = ()=>{
+    return (
+      <div className='text-center'>
+        <img src={Skate} alt="loading"/>
+      </div>
+    )
+  
+}
+export default Spinner
+
+
+
